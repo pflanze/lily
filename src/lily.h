74bdaf5e0b891727960f0096481f2683117fd24b
Christian Jaeger <ch@christianjaeger.ch>
2018-08-17 17:06:35 +0100

    lily.h: use shared_ptr, separate LilyPair and LilyListPair

--- src/lily.h
+++ src/lily.h
@@ -1,36 +1,67 @@
+#include <std::string>
+#include <std::shared_ptr>
+
+class LilyObject;
+class LilyList;
+
+typedef std::shared_ptr<LilyObject> LilyObjectPtr;
+typedef std::shared_ptr<LilyList> LilyListPtr;
 
 class LilyObject {
-
+	virtual LilyObjectPtr eval(LilyObjectPtr v, LilyListPtr ctx);
 };
 
-enum LilySpecialKind {
+enum class LilySpecialKind {
 	FALSE,
 	TRUE,
-	NIL,
 };
 
 class LilySpecial : LilyObject {
 	int kind;
 };
 
+class LilyList {
+	virtual LilyObjectPtr first() = 0;
+	virtual bool isNull() = 0;
+};
+
+class LilyNull : LilyList {
+	virtual LilyObjectPtr first() {
+		throw std::logic_error("end of list");
+	};
+	virtual bool isNull() { return true; }
+	static LilyObjectPtr lilyNull() {
+		static LilyObjectPtr v (new LilyNull());
+		return v;
+	}
+};
+
+class LilyListPair : LilyList {
+	LilyObjectPtr _first;
+	LilyListPtr _rest;
+	virtual LilyObjectPrt first() { return _first; }
+	virtual LilyListPtr rest() { return _rest; }
+	virtual bool isNull() { return false; }
+};
+
 class LilyPair : LilyObject {
-	LilyObject* car;
-	LilyObject* cdr;
+	LilyObjectPtr fst;
+	LilyObjectPtr snd;
 };
 
 class LilyString : LilyObject {
-	std::string value;
+	std::string string;
 };
 
 class LilySymbol : LilyObject {
-	std::string value;
+	std::string string;
 };
 
 class LilyNumber : LilyObject {
 };
 
 class LilyInt64 : LilyNumber {
-	int64 value;
+	int64_t value;
 };
 
 class LilyDouble : LilyNumber {
