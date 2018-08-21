23de159870aae0c824d9b5879538f30fb243493a
Christian Jaeger <ch@christianjaeger.ch>
2018-08-21 01:26:43 +0100

    t3: remove demonstration code

--- test/t3.cpp
+++ test/t3.cpp
@@ -15,52 +15,8 @@ void e(const char* codestring) {
 	std::cout << result->typeName() << ": "; WRITELN(result);
 }
 
-void weirdtest(LilyListPtr mylist) {
-	// (22 23 23455)
-
-	// auto myvalue= PAIR(INT(22), PAIR(INT(23), PAIR(INT(23445),  NIL)));
-
-	auto mylistp= LIST_UNWRAP(mylist);
-	while (mylistp->isPair()) {
-		//XX add layer that has 'value' method--ah can't, return type
-		std::cout <<
-			UNWRAP_AS(LilyInt64, mylistp->first())->value
-			// XX^ ugly, should provide value method
-			  << "\n";
-		mylistp= LIST_UNWRAP(mylistp->rest());
-	}
-
-	WRITELN(BOOLEAN((new LilyPair(INT(33), INT(444)))-> isPair()));
-
-	WRITELN(BOOLEAN((new LilyNull())-> isPair()));
-}
-
-#define IS(type, var)  !!dynamic_cast<type*>(&*(var))
-
-// bool handdispatch_to_isPair(LilyListPtr v) {
-// 	if (IS(LilyPair, v)) {
-// 		return true; 
-// 	} else {
-// 		return false;
-// 	}
-// }
-
-// bool isPair(LilyPair* v) {
-// 	return true;
-// }
-// bool isPair(LilyNull* v) {
-// 	return false;
-// }
-// -> doesn't actually work because C++ doesn't check the runtime type
-//    information for overloaded functions, only static types.
-
 
 int main () {
-	note("weirdtest"); // whatever name
-	weirdtest(LIST_PAIR(INT(22),
-			    LIST_PAIR(INT(23),
-				      LIST_PAIR(INT(23445),
-						NIL))));
 	note("addition");
 	e("(+)"); // 0
 	e("(+ 10 20)"); // 30
