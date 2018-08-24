0975f2cfe2f7d833836428214240985d8e163b7b
Christian Jaeger <ch@christianjaeger.ch>
2018-08-24 03:01:48 +0100

    lilyDefaultEnvironment: add |-|, |quotient|, |modulo|

--- src/lilyDefaultEnvironment.cpp
+++ src/lilyDefaultEnvironment.cpp
@@ -24,25 +24,51 @@ T _lilyFold(LilyList* vs, std::function<T(LilyT*,T)> fn, T start) {
 	}
 }
 
-#define DEF_PRIMITIVE(name, LilyT, T, OP)				\
+#define DEF_UP_PRIMITIVE(name, LilyT, T, OP, START)			\
 	static								\
 	LilyObjectPtr name(LilyObjectPtr vs) {				\
-		return INT((_lilyFold<LilyT, T>				\
-			    (XLIST_UNWRAP(vs),				\
-			     [](LilyT* num, T res) -> T {		\
-				    /* XX check for overflow! */	\
-				    return num->value OP res;		\
-			    }, 0)));					\
+		return LILY_NEW						\
+			(LilyT(_lilyFold<LilyT, T>			\
+			       (XLIST_UNWRAP(vs),			\
+				[](LilyT* num, T res) -> T {		\
+				       /* XX check for overflow! */	\
+				       return res OP num->value;	\
+			        },					\
+				START)));				\
 	}
 
-DEF_PRIMITIVE(lilyAdd, LilyInt64, int64_t, +);
-DEF_PRIMITIVE(lilyMult, LilyInt64, int64_t, *);
+DEF_UP_PRIMITIVE(lilyAdd, LilyInt64, int64_t, +, 0);
+DEF_UP_PRIMITIVE(lilyMult, LilyInt64, int64_t, *, 1);
+
+#define DEF_DOWN_PRIMITIVE(name, LilyT, T, OP)				\
+	static								\
+	LilyObjectPtr name(LilyObjectPtr vs) {				\
+		LilyList* _vs= XLIST_UNWRAP(vs);			\
+		return LILY_NEW						\
+			(LilyT(_lilyFold<LilyT, T>			\
+			       (XLIST_UNWRAP(_vs->rest()),		\
+				[](LilyT* num, T res) -> T {		\
+				       /* XX check for overflow? */	\
+				       return res OP num->value;	\
+			        },					\
+				XUNWRAP_AS(LilyT, _vs->first())->value))); \
+	}
+
+DEF_DOWN_PRIMITIVE(lilySub, LilyInt64, int64_t, -);
+DEF_DOWN_PRIMITIVE(lilyQuotient, LilyInt64, int64_t, /);
+DEF_DOWN_PRIMITIVE(lilyModulo, LilyInt64, int64_t, %);
+
+// DEF_PRIMITIVE(lilyDiv, LilyInt64, int64_t, /); // generic
 
 
 LilyListPtr lilyDefaultEnvironment() {
 	LilyListPtr env= LIST(
 		PAIR(SYMBOL("+"), PRIMITIVE(lilyAdd, "+")),
 		PAIR(SYMBOL("*"), PRIMITIVE(lilyMult, "*")),
+		PAIR(SYMBOL("-"), PRIMITIVE(lilySub, "-")),
+		PAIR(SYMBOL("quotient"), PRIMITIVE(lilyQuotient, "quotient")),
+		PAIR(SYMBOL("modulo"), PRIMITIVE(lilyModulo, "modulo")),
+		// PAIR(SYMBOL("integer./"), PRIMITIVE(lilyDiv, "integer./")),
 		);
 	return env;
 }
