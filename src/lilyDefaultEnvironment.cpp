96d5e1bbcf74d8aefa01f6ed9d58f84f384afef9
Christian Jaeger <ch@christianjaeger.ch>
2018-08-24 02:31:38 +0100

    lilyDefaultEnvironment: implement |*|

--- src/lilyDefaultEnvironment.cpp
+++ src/lilyDefaultEnvironment.cpp
@@ -24,38 +24,19 @@ T _lilyFold(LilyList* vs, std::function<T(LilyT*,T)> fn, T start) {
 	}
 }
 
-// #define DEF_PRIMITIVE(name, 
-
-static
-LilyObjectPtr lilyAdd(LilyObjectPtr vs) {
-	//XXX XLIST_UNWRAP
-	return INT((_lilyFold<LilyInt64, int64_t>
-		    (XLIST_UNWRAP(vs),
-		     [](LilyInt64* num, int64_t res) -> int64_t {
-			    return num->value + res; // XX check for overflow!
-		    }, 0)));
-}
-
-// XX stupid copypaste
-static
-LilyObjectPtr lilyMult(LilyObjectPtr vs) {
-	int64_t total=1;
-	LilyObject* p= UNWRAP(vs);
-	while (true) {
-		if (auto pair= dynamic_cast<LilyPair*>(p)) {
-			if (auto num=  UNWRAP_AS(LilyInt64, pair->_car)) {
-				total*= num->value; // XX check for overflow!
-				p= UNWRAP(pair->_cdr);
-			} else {
-				throw std::logic_error("not an integer");
-			}
-		} else if (dynamic_cast<LilyNull*>(p)) {
-			return INT(total);
-		} else {
-			throw std::logic_error("not a proper list");
-		}
+#define DEF_PRIMITIVE(name, LilyT, T, OP)				\
+	static								\
+	LilyObjectPtr name(LilyObjectPtr vs) {				\
+		return INT((_lilyFold<LilyT, T>				\
+			    (XLIST_UNWRAP(vs),				\
+			     [](LilyT* num, T res) -> T {		\
+				    /* XX check for overflow! */	\
+				    return num->value OP res;		\
+			    }, 0)));					\
 	}
-}
+
+DEF_PRIMITIVE(lilyAdd, LilyInt64, int64_t, +);
+DEF_PRIMITIVE(lilyMult, LilyInt64, int64_t, *);
 
 
 LilyListPtr lilyDefaultEnvironment() {
