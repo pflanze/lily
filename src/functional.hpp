ef17ae56ed7d82c50556a3656f6a11ab9bf87b04
Christian Jaeger <ch@christianjaeger.ch>
2018-09-12 01:20:28 +0100

    functional.hpp: generate the complement code via macro to make it work

--- src/functional.hpp
+++ src/functional.hpp
@@ -3,12 +3,21 @@
 
 #include <functional>
 
-template <typename T>
-std::function<bool(T)> complement(std::function<bool(T)> fn) {
-	return [&](T v) -> bool {
-		return ! fn(v);
-	};
-}
+// template <typename T>
+// std::function<bool(T)> complement(std::function<bool(T)> fn) {
+// 	return [&](T v) -> bool {
+// 		return ! fn(v);
+// 	};
+// }
+
+// #define COMPLEMENT(T, e) complement<T>(static_cast<std::function<bool(T)>>(e))
+
+// Doesn't work with overloaded functions as fn; what's the problem,
+// lame?
+
+#define COMPLEMENT(T, fnsym) [&](T v) -> bool {			\
+		return ! fnsym(v);				\
+	}
 
 
 #endif // _FUNCTIONAL_HPP
